a5d5f89149e7734846489e494c9bae6cf6bd79bc
Aissatouba <bouna062000@gmail.com>
2021-11-08 14:18:29 +0000

    fisrt commit

---
+++ js/dash.js
@@ -0,0 +1,9 @@
+function toogleMenu(){
+    let toggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
+
+    toggle.classList.toggle('active');
+    navigation.classList.navigation('active');
+    main.classList.main('active');
+}
